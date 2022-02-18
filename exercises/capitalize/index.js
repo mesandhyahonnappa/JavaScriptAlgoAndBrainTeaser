9baa5a3e525aac1be9ed45b737c977b6c8bedc7e
sandhyahonnappa <me@sandhyahonnappa.dev>
2022-02-18 22:10:51 +0530

    capitalize

---
+++ exercises/capitalize/index.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize(str) {
+  return str
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.substring(1))
+    .join(" ");
+}
+
+module.exports = capitalize;
