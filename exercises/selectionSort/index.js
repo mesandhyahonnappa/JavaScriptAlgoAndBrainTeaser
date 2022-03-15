d93b4946eae542a02b4bcb04d5f6c4bc7d84bb4e
sandhyahonnappa <me@sandhyahonnappa.dev>
2022-03-15 06:55:37 +0530

    Selection Sort fix

--- exercises/selectionSort/index.js
+++ exercises/selectionSort/index.js
@@ -1,18 +1,16 @@
 function sort(arr) {
-  let hasSwapped = false;
   for (let i = 0; i < arr.length; i++) {
-    hasSwapped = false;
     let minIndex = i;
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] < arr[minIndex]) {
         minIndex = j;
-        hasSwapped = true;
       }
     }
-    if (hasSwapped) {
+    if (i !== minIndex) {
       let temp = arr[i];
       arr[i] = arr[minIndex];
       arr[minIndex] = temp;
+      console.log(arr);
     }
   }
   return arr;
